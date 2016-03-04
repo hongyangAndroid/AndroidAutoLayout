715026a0c170057c310cae906a9242d4573b4816
hongyangAndroid <lmj623565791@gmail.com>
2016-03-04 14:23:12 +0800

    add AutoRadioGroup,AutoTableLayout,AutoTableRow

---
+++ autolayout-widget/src/main/java/com/zhy/autolayout/widget/AutoTableLayout.java
@@ -0,0 +1,86 @@
+package com.zhy.autolayout.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+
+import com.zhy.autolayout.AutoLayoutInfo;
+import com.zhy.autolayout.utils.AutoLayoutHelper;
+
+/**
+ * Created by hupei on 2016/2/29 9:59.
+ */
+public class AutoTableLayout extends TableLayout
+{
+    private AutoLayoutHelper mHelper = new AutoLayoutHelper(this);
+
+    public AutoTableLayout(Context context)
+    {
+        super(context);
+    }
+
+    public AutoTableLayout(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        if (!isInEditMode())
+            mHelper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs)
+    {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+
+    public static class LayoutParams extends TableLayout.LayoutParams
+            implements AutoLayoutHelper.AutoLayoutParams
+    {
+        private AutoLayoutInfo mAutoLayoutInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs)
+        {
+            super(c, attrs);
+            mAutoLayoutInfo = AutoLayoutHelper.getAutoLayoutInfo(c, attrs);
+        }
+
+        @Override
+        public AutoLayoutInfo getAutoLayoutInfo()
+        {
+            return mAutoLayoutInfo;
+        }
+
+
+        public LayoutParams(int width, int height)
+        {
+            super(width, height);
+        }
+
+
+        public LayoutParams(ViewGroup.LayoutParams source)
+        {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source)
+        {
+            super(source);
+        }
+
+    }
+}
---
+++ autolayout-widget/src/main/java/com/zhy/autolayout/widget/AutoTableRow.java
@@ -0,0 +1,87 @@
+package com.zhy.autolayout.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.TableRow;
+
+import com.zhy.autolayout.AutoLayoutInfo;
+import com.zhy.autolayout.utils.AutoLayoutHelper;
+
+/**
+ * Created by hupei on 2016/2/29 9:59.
+ */
+public class AutoTableRow extends TableRow
+{
+    private AutoLayoutHelper mHelper = new AutoLayoutHelper(this);
+
+    public AutoTableRow(Context context)
+    {
+        super(context);
+    }
+
+    public AutoTableRow(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        if (!isInEditMode())
+            mHelper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
+        super.onLayout(changed, l, t, r, b);
+    }
+
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs)
+    {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+
+    public static class LayoutParams extends TableRow.LayoutParams
+            implements AutoLayoutHelper.AutoLayoutParams
+    {
+        private AutoLayoutInfo mAutoLayoutInfo;
+
+        public LayoutParams(Context c, AttributeSet attrs)
+        {
+            super(c, attrs);
+            mAutoLayoutInfo = AutoLayoutHelper.getAutoLayoutInfo(c, attrs);
+        }
+
+        @Override
+        public AutoLayoutInfo getAutoLayoutInfo()
+        {
+            return mAutoLayoutInfo;
+        }
+
+
+        public LayoutParams(int width, int height)
+        {
+            super(width, height);
+        }
+
+
+        public LayoutParams(ViewGroup.LayoutParams source)
+        {
+            super(source);
+        }
+
+        public LayoutParams(MarginLayoutParams source)
+        {
+            super(source);
+        }
+
+    }
+}
